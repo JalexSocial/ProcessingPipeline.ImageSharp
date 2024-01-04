b042470c6993a4c52105e4bd2cbcc661200a49fb
Andrew Xia <xiathschool@gmail.com>
2024-01-04 08:34:12 -0500

    Refactor image resizing logic and improve code readability
    - Removed unnecessary comments and unused code
    - Simplified the calculation of the final image size using aspect ratio and width
    - Reorganized the code to resize the foreground image before forming the background image
    - Set the background location based on the input image's aspect ratio

--- ProcessingPipeline.ImageSharp/ImageProcessingPipelineService.cs
+++ ProcessingPipeline.ImageSharp/ImageProcessingPipelineService.cs
@@ -14,75 +14,53 @@ public class ImageProcessingPipelineService
 {
     public Image SmartResize(Image image, AspectRatio ratio, int width)
     {
-        // Resize size into correct aspect ratio. Might be able to throw error here in the future if size does not match
+        // Forming the final image size using aspect ratio and width
         Size size = new Size(width, 0);
         size.Height = size.Width / ratio.Width * ratio.Height;
         
+        // Resizing the foreground image to the requested size
         var foreground = image.Clone(fg => fg.Resize(new ResizeOptions
         {
             Mode = ResizeMode.Max,
             Size = size,
         }));
 
-        Image finalImage = image;
+        // Depending on the input image's aspect ratio, set the background location accordingly
+        Point backgroundLocation;
         
+        // The input image is wider than requested
         if (image.Width * size.Height > image.Height * size.Width)
         {
-            // Removed `using`
-            /*Image compositeImage = image.Clone(img => img
-                .Resize(new ResizeOptions
-                {
-                    Mode = ResizeMode.Stretch,
-                    Size = size,
-                })
-                .GaussianBlur(100f)
-                .Brightness(0.5f)
-                .DrawImage(foreground, new Point(0, size.Height / 2 - foreground.Height / 2), 1f)
-            );
-            finalImage = compositeImage;*/
-            
-            // Removed `using`
-            Image compositeImage = image.Clone(img => img
-                .Resize(new ResizeOptions
-                {
-                    Mode = ResizeMode.Stretch,
-                    Size = new Size(300, 300),
-                })
-                .GaussianBlur(50f)
-                .Resize(new ResizeOptions
-                {
-                    Mode = ResizeMode.Stretch,
-                    Size = size,
-                })
-                .Brightness(0.5f)
-                .DrawImage(foreground, new Point(0, size.Height / 2 - foreground.Height / 2), 1f)
-                
-            );
-            finalImage = compositeImage;
+            backgroundLocation = new Point(0, size.Height / 2 - foreground.Height / 2);
         } 
+        // The input image is taller than requested
         else if (image.Width * size.Height < image.Height * size.Width)
         {
-            Image compositeImage = image.Clone(img => img
-                .Resize(new ResizeOptions
-                {
-                    Mode = ResizeMode.Stretch,
-                    Size = new Size(300, 300),
-                })
-                .GaussianBlur(50f)
-                .Resize(new ResizeOptions
-                {
-                    Mode = ResizeMode.Stretch,
-                    Size = size,
-                })
-                .Brightness(0.5f)
-                .DrawImage(foreground, new Point(size.Width / 2 - foreground.Width / 2, 0), 1f)
-            );
-            finalImage = compositeImage;
+            backgroundLocation = new Point(size.Width / 2 - foreground.Width / 2, 0);
         }
+        // The input image is already the correct aspect ratio
         else
         {
-            
+            backgroundLocation = new Point(0, 0);
         }
-        return finalImage;
+        
+        // Forming the background image before drawing the foreground above it
+        Image compositeImage = image.Clone(img => img
+            .Resize(new ResizeOptions
+            {
+                Mode = ResizeMode.Stretch,
+                Size = new Size(300, 300),
+            })
+            .GaussianBlur(50f)
+            .Resize(new ResizeOptions
+            {
+                Mode = ResizeMode.Stretch,
+                Size = size,
+            })
+            .Brightness(0.5f)
+            .DrawImage(foreground, backgroundLocation, 1f)
+        );
+        
+        return compositeImage;
     }
 }
